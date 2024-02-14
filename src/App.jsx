89f8c68c9f697c7be4a61020ba72ab47f1c4f736
Om Patel <omupatel7113@gmail.com>
2024-02-14 13:00:32 +0530

    Speed Insights Added

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,11 @@ import AdminLogin from "./components/AdminLogin/AdminLogin";
 import MyState from "./context/myState";
 import GoToTop from "./components/goTop/GoToTop";
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 
 const App = () => {
   inject();
+  injectSpeedInsights();
 
   return (
     <>
